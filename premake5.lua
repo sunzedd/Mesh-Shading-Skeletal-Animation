f063cf9d9a804fd5a85f9010bcc699e491ecbd82
Rostislav V <sunz89@yandex.ru>
2021-05-15 00:02:22 +0300

    Add basic project setup
    -Setup premake5 build file.
    -Created src directory and main.cpp file with 'Hello Triangle' demo
     using GLAD, GLFW3, GLM libraries.
    -Added dependecies folder.
    -Updated gitignore.

---
+++ premake5.lua
@@ -0,0 +1,47 @@
+workspace "Mesh-Shaders-Skeletal-Animation"
+    architecture "x86_64"
+
+    configurations
+    {
+        "Debug"
+    }
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+    project "Application"
+        kind "ConsoleApp"
+        language "C++"
+        cppdialect "C++17"
+        staticruntime "off"
+
+        targetdir("%{wks.location}/build/bin/" .. outputdir .. "-%{prj.name}")
+        objdir ("%{wks.location}/build/obj/" .. outputdir .. "-%{prj.name}")
+
+        files
+        {
+            "src/**.h",
+            "src/**.cpp",
+            "deps/glad/src/glad.c"
+        }
+
+        includedirs
+        {
+            "%{wks.location}/deps/glad/include",
+            
+            "D:/Library/GLFW/glfw-3.3.4/include",
+            "D:/Library/glm"
+        }
+
+        libdirs
+        {
+            "D:/Library/GLFW/glfw-3.3.4.bin.WIN64/lib-vc2019"
+        }
+
+        links
+        {
+            "glfw3.lib"
+        }
+
+        filter "configurations:Debug"
+            symbols "on"
+            defines "DEBUG"
