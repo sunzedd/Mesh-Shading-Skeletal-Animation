61a70ff57ee110fb95c09408b9e27398f5028659
Rostislav V <sunz89@yandex.ru>
2021-06-10 17:39:51 +0300

    Add cone (float[4]) member to meshlet structure

--- src/Engine/MeshShading/Meshlet.h
+++ src/Engine/MeshShading/Meshlet.h
@@ -5,6 +5,7 @@ namespace FQW {
 
 struct Meshlet
 {
+    vec4 cone;
     uint32_t vertices[64];       // vertex indices in mesh vertexbuffer
     uint8_t indices[126*3];    // vertex indices in meshlet's vertexbuffer (Meshlet.vertices)
     uint8_t triangleCount = 0;
