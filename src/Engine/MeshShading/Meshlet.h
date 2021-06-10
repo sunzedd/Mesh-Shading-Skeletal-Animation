7ccdf6fcfd5b46d98cbda6993b24d9f9b5128287
Rostislav V <sunz89@yandex.ru>
2021-06-10 17:23:40 +0300

    Use uint8_t for meshlet structure

--- src/Engine/MeshShading/Meshlet.h
+++ src/Engine/MeshShading/Meshlet.h
@@ -6,9 +6,9 @@ namespace FQW {
 struct Meshlet
 {
     uint32_t vertices[64];       // vertex indices in mesh vertexbuffer
-    uint32_t indices[126*3];    // vertex indices in meshlet's vertexbuffer (Meshlet.vertices)
-    uint32_t triangleCount = 0;
-    uint32_t vertexCount = 0;
+    uint8_t indices[126*3];    // vertex indices in meshlet's vertexbuffer (Meshlet.vertices)
+    uint8_t triangleCount = 0;
+    uint8_t vertexCount = 0;
 };
 
 } // namespace FQW::MeshShaders
